ed984e063578bb547c4f17efe5f595a4932351c3
ankrishx96 <ankrishkandpal1996@gmail.com>
2025-04-27 10:17:42 +0530

    Adding FactorialNew Method

--- factorial.js
+++ factorial.js
@@ -3,8 +3,12 @@
 
 function factorial(n, factRes = 1) {
   if (n === 0 || n === 1) return factRes;
-
   return factorial(n - 1, factRes * n);
+}
+
+function factorialNew(n) {
+  if (n === 0 || n === 1) return n;
+  return n * factorial(n - 1);
 }
 
 const res = factorial(5);
